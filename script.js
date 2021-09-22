bf63f340272ae403b28bc2c4abebcb3bb5aae0fd
Anara Kulmambetova <anara.akimkanova.kk@gmail.com>
2021-09-22 04:12:32 +0400

    home work js 4 - 2 task only

---
+++ script.js
@@ -0,0 +1,34 @@
+//task1
+
+let sum = prompt(`n*(n+1)/2=`);
+let sum2 = Number(+sum+1);
+let sum3 = Number(sum2*sum);
+let sum4 = Number(+sum3/2);
+
+document.write(`n*(n+1)/2= ${sum4}<br>`);
+
+
+
+//task2
+
+
+let a = prompt(`suvenire`);
+let b = prompt(`bezdelushka`); 
+let suv = a*75;
+let bez = b*112;
+let summ = Number (+ suv+bez);
+
+document.write(a +" - suvenire <br>");
+document.write(b +" - bezdelushka <br>");
+document.write(suv.toFixed(2) +" gramm <br>");
+document.write(bez.toFixed(2) +" gramm<br>");
+document.write(summ.toFixed(2) +" gramm<br>");
+
+
+
+
+
+
+
+
+
